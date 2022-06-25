1723c5002a3010377853b6913da4398781eca8ee
Jack Anderson <jack@jackanderson.dev>
2022-06-24 23:44:23 -0400

    add in barlow font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,6 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="h-screen grid place-content-center bg-gray-300">
+	<h1 class="text-5xl text-blue-600 font-barlow">Welcome to SvelteKit</h1>
+	<p class="text-xl text-red-500">
+		Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	</p>
+</div>
