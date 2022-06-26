8bcdb73feae666e51e3f5531df559dbf2357ebb8
Jack Anderson <jack@jackanderson.dev>
2022-06-26 17:45:59 -0500

    don't use svelte head

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -1,5 +1,3 @@
+<script defer src="https://cdn.commento.io/js/commento.js"></script>
+
 <div id="commento" />
-<svelte:head>
-	<script src="https://cdn.commento.io/js/commento.js">
-	</script>
-</svelte:head>
