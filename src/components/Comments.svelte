ead6db4d7f4bd6b67f15d866184f53c6cdb209f3
Jack Anderson <jack@jackanderson.dev>
2022-06-26 17:24:38 -0500

    try hyvor

---
+++ src/components/Comments.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<script src="https://cdn.commento.io/js/commento.js">
+	</script>
+</svelte:head>
+
+<div id="commento" />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,15 @@
 </script>
 
 <svelte:head>
-	<script src="https://cdn.commento.io/js/commento.js"></script>
+	<div id="hyvor-talk-view" />
+	<script type="text/javascript">
+		var HYVOR_TALK_WEBSITE = 7312;
+		var HYVOR_TALK_CONFIG = {
+			url: false,
+			id: false
+		};
+	</script>
+	<script async type="text/javascript" src="//talk.hyvor.com/web-api/embed.js"></script>
 </svelte:head>
 
 <slot />
