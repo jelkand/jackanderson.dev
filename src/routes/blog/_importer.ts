1fa4f6ae7893b555bc9209ed0e9c1383357dd4c7
Jack Anderson <jack@jackanderson.dev>
2022-07-09 18:54:36 -0400

    kludgy fix to promises

--- src/routes/blog/_importer.ts
+++ src/routes/blog/_importer.ts
@@ -1,5 +1,3 @@
-import type { Post } from 'src/types/Post';
-
 export type Metadata = {
 	title: string;
 	date: string;
@@ -9,19 +7,4 @@ export type Metadata = {
 };
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const _pathToSlug = (path: string) => path.match(/\.\/(.*)\.svx/)![1]!;
-
-export const allPosts: Post[] = await (async () => {
-	const modules = import.meta.glob<{
-		metadata: Metadata;
-	}>('./*.svx');
-	return Promise.all(
-		Object.entries(modules).map(async ([path, val]) => {
-			const {
-				metadata: { title, description, date: rawDate, category, tags }
-			} = await val();
-			const slug = _pathToSlug(path);
-			return { slug, title, description, date: new Date(rawDate), category, tags };
-		}, [] as Post[])
-	);
-})();
+export const _pathToSlug = (path: string) => path.match(/\.\/(.*)\.svx/)![1]!;
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
 	import { compareDesc } from 'date-fns';
 	import PostCard from 'src/components/PostCard.svelte';
-	import { allPosts } from './_importer';
+	import type { Post } from 'src/types/Post';
+	import { _pathToSlug, type Metadata } from './_importer';
 
-	$: sortedPosts = allPosts.sort((a, b) => compareDesc(a.date, b.date));
+	const modules = import.meta.glob<{
+		metadata: Metadata;
+	}>('./*.svx');
+
+	const sortedPostsPromise = (async () => {
+		const posts = await Promise.all(
+			Object.entries(modules).map(async ([path, val]) => {
+				const {
+					metadata: { title, description, date: rawDate, category, tags }
+				} = await val();
+				const slug = _pathToSlug(path);
+				return { slug, title, description, date: new Date(rawDate), category, tags };
+			}, [] as Post[])
+		);
+
+		return posts.sort((a, b) => compareDesc(a.date, b.date));
+	})();
 </script>
 
 <div class="flex place-content-center">
 	<ul>
-		{#each sortedPosts as post}
-			<PostCard {post} />
-		{/each}
+		{#await sortedPostsPromise then sortedPosts}
+			{#each sortedPosts as post}
+				<PostCard {post} />
+			{/each}
+		{/await}
 	</ul>
 </div>
