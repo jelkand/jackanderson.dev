a933a6cd670c92df20a12ab168c3aeb0f448f1f8
Jack Anderson <jack@jackanderson.dev>
2022-10-16 16:50:59 -0400

    chore: cleanup layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,6 @@
 	import '../prism-material-dark.css';
 	import { name } from '$lib/info';
 
-	import { fly } from 'svelte/transition';
-	import { MenuIcon, XIcon } from 'svelte-feather-icons';
-
 	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 
 	const headerLinks = [
@@ -21,10 +18,6 @@
 <div class="flex flex-col min-h-screen">
 	<div class="flex flex-col flex-grow w-full px-4 py-2">
 		<header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
-			<!-- <div class="flex h-16 px-4 py-2 justify-between items-center"> -->
-			<!-- <h2
-					class="mr-auto !text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-500 dark:from-violet-500 dark:to-pink-500"
-				> -->
 			<a
 				class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-teal-600 dark:to-teal-400"
 				href="/"
@@ -33,41 +26,7 @@
 			</a>
 
 			<ThemeToggle />
-			<!-- </h2> -->
-
-			<!-- <div class="flex-initial items-center m-3  md:block hidden">
-					{#each headerLinks as link}
-						<a class="m-1" href={link.url}>{link.label}</a>
-					{/each}
-				</div> -->
-			<!-- <div class="flex-initial items-center md:hidden m-3">
-					<button class="flex items-center" on:click={() => (menuOpen = true)}>
-						<MenuIcon />
-					</button>
-				</div> -->
-			<!-- </div> -->
-			<!-- Mobile menu -->
-			<!-- {#if menuOpen}
-				<div
-					class="h-full bg-white dark:bg-slate-600 z-40 fixed top-0 right-0 w-48 flex flex-col"
-					transition:fly={{ x: 192, duration: 200 }}
-				>
-					<div class="flex justify-end w-full">
-						<button on:click={() => (menuOpen = false)}>
-							<XIcon size="2x" />
-						</button>
-					</div>
-					<div class="flex flex-col items-start ">
-						{#each headerLinks as link}
-							<a class="m-4 text-lg text-right" href={link.url} on:click={() => (menuOpen = false)}
-								>{link.label}</a
-							>
-						{/each}
-					</div>
-				</div>
-			{/if} -->
 		</header>
-		<!-- on:click={() => (menuOpen = false)} -->
 		<main class="flex flex-col flex-grow w-full max-w-2xl mx-auto">
 			<slot />
 		</main>
